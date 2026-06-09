954dfae4366849acd944002e2d85b8d7213d7566
ilbarsseckin <ilbarsseckin@gmail.com>
2026-06-09 18:05:23 +0300

    fix: API URL fallback guncellendi

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,`n  output: 'standalone',
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8082',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '128.140.127.151:8081',
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'https://smartdiafon.com.tr',
   },
   async redirects() {
