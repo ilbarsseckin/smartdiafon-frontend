58a936862812b53c5c71cbe93dc15a952bb631b3
ilbars <ilbarsseckin@gmail.com>
2026-06-14 21:43:54 +0000

    fix: HizliTeklif anasayfaya geri eklendi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,6 +93,7 @@ export default function Home() {
         <ReferencesSection />
         <GuvenRozetleri />
         <KampanyaSerit />
+        <HizliTeklif />
         <Uyumluluk />
         <Favorilerim />
         <UrunlerSection />
